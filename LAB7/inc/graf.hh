fdb1921c832d55187660db7cf85456e0fccf29bd
psiejka <218730@student.pwr.edu.pl>
2016-04-29 15:18:52 +0200

    Poczatki cwiczenia nr 7

---
+++ LAB7/inc/graf.hh
@@ -0,0 +1,23 @@
+#ifndef _GRAF_HH
+#define _GRAF_HH
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+class IGraf{
+public:
+	
+	};
+
+class Graf{
+private:
+	
+	
+public:
+	
+	};
+
+#endif
---
+++ LAB7/inc/szukajgrafu.hh
@@ -0,0 +1,19 @@
+#ifndef _SZUKAJGRAFU_HH
+#define _SZUKAJGRAFU_HH
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include "../inc/graf.hh"
+
+using namespace std;
+
+class SzukajGrafu{
+private:
+	
+public:
+	
+	
+	};
+
+#endif
